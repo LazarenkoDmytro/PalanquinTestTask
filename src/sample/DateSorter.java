98216e3f79a640744150140af9b93dfb62241fd8
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-06-13 03:35:39 +0300

    Initial commit

---
+++ src/sample/DateSorter.java
@@ -0,0 +1,25 @@
+package sample;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Provides functionality to sort dates based on specific criteria.
+ * Dates with an 'r' in the month are sorted in ascending order,
+ * and then dates without an 'r' in the month are sorted in descending order.
+ */
+public class DateSorter {
+
+    /**
+     * Sorts a list of LocalDate objects according to the specified criteria.
+     * Dates with an 'r' in the month are sorted in ascending order,
+     * and then dates without an 'r' in the month are sorted in descending order.
+     *
+     * @param unsortedDates an unsorted list of dates
+     * @return the collection of dates sorted according to the specified criteria
+     */
+    public Collection<LocalDate> sortDates(List<LocalDate> unsortedDates) {
+        // Solution here
+    }
+}
