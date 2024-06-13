9add5969abb4b522c3cb53ceb3dc505d6a1d63b2
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-06-13 03:56:09 +0300

    The method to run and test

--- src/sample/DateSorter.java
+++ src/sample/DateSorter.java
@@ -54,4 +54,15 @@ public class DateSorter {
 
         return sortedDates;
     }
+
+    // Run this method to test the code
+    public static void main(String[] args) {
+        List<LocalDate> unsortedDates = new ArrayList<>();
+        unsortedDates.add(LocalDate.of(2004, 7, 1));
+        unsortedDates.add(LocalDate.of(2005, 1, 2));
+        unsortedDates.add(LocalDate.of(2007, 1, 1));
+        unsortedDates.add(LocalDate.of(2032, 5, 3));
+
+        System.out.println(new DateSorter().sortDates(unsortedDates));
+    }
 }
